a1f5b941105c2ab9c34540a486d6ded24e69d43a
Rafael Batista <RafaelB24@yahoo.com>
2019-05-11 02:20:38 -0400

    Add omniauth facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'},
+  controllers: { omniauth_callbacks: 'users' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'static_pages#home'
 end
