e0cdbdc52d68bd997a1a2f8402935189a692a2b7
Rafael Batista <RafaelB24@yahoo.com>
2019-05-13 19:34:28 -0700

    Add Item model, add tests for Item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_021145) do
+ActiveRecord::Schema.define(version: 2019_05_14_023127) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
