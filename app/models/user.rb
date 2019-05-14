088a0e99568f42329cfdda42e29671b059acd13b
Rafael Batista <RafaelB24@yahoo.com>
2019-05-14 16:05:14 -0700

    Add index checker to lib/tasks, add user validations, pass user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
+  validates :username, :presence => true
+  has_many :recipes
 end
