43154f1e5f1ad849d89f3b689f571e472d5301b5
Rafael Batista <RafaelB24@yahoo.com>
2019-05-15 22:44:02 -0700

    Add unique validation to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
-  validates :username, :presence => true
+  validates :username, :presence => true, :uniqueness => {case_sensitive: false}
   has_many :recipes
   has_many :recipe_ratings
 end
