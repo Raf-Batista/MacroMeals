6dacf0ed8aae15cc433cf9c3b9e292c0027bc527
Rafael Batista <RafaelB24@yahoo.com>
2019-05-15 22:43:10 -0700

    Add User test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,10 @@ RSpec.describe User, type: :model do
   it {should have_many(:recipes)}
 
   it {should have_many(:recipe_ratings)}
+
+  it 'should not create a user with the same username' do
+    User.create(:email => 'test@example.com', :username => 'test', :password => 'test123')
+    user = User.create(:email => 'test3@example.com', :username => 'test', :password => 'test123')
+    expect(user.valid?).to eq(false)
+  end
 end
