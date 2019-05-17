d1f0c05f930ecdcd570e1154737c3d1335101a5d
Rafael Batista <RafaelB24@yahoo.com>
2019-05-16 20:12:13 -0700

    Add test for adding to session hash on sign up

--- spec/features/users_features_spec.rb
+++ spec/features/users_features_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe 'Users features', :type => :feature do
     expect(User.last.username).to eq('test')
     expect(page).to have_content('Signed Up Successfully')
   end
+
+  it 'adds a session hash on Sign Up' do
+    visit new_user_path
+    fill_in 'user[username]', :with => 'test'
+    fill_in 'user[password]', :with => 'test123'
+    click_button 'Sign Up'
+    expect(page.get_rack_session['user_id']).to_not eq(nil)
+  end
+
 end
