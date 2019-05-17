7ce357619294208a48fbb75853057293a07ea1a8
Rafael Batista <RafaelB24@yahoo.com>
2019-05-16 20:04:01 -0700

    Add User new and create action, User new view, flash message for successful login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,17 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.create(user_params)
+    redirect_to root_path, :flash => {:message => 'Signed Up Successfully'}
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
--- spec/features/users_features_spec.rb
+++ spec/features/users_features_spec.rb
@@ -4,8 +4,8 @@ require 'rails_helper'
 RSpec.describe 'Users features', :type => :feature do
   it 'signs up sucessfully' do
     visit new_user_path
-    fill_in 'Username', :with => 'test'
-    fill_in 'Password', :with => 'test123'
+    fill_in 'user[username]', :with => 'test'
+    fill_in 'user[password]', :with => 'test123'
     click_button 'Sign Up'
     expect(User.last.username).to eq('test')
     expect(page).to have_content('Signed Up Successfully')
