0d48e92c738db6e8833da1e56d648b49843e825a
Rafael Batista <RafaelB24@yahoo.com>
2019-05-16 22:45:23 -0700

    Add #current_user and #logged_in? to Application controller, add redirect to show page if logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
   def logout
     session.clear
   end
+
+  def current_user
+    User.find(session[:user_id])
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end
 end
