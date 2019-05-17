358d4f54c9a37a6f2504c871afa6ac242140ff22
Rafael Batista <RafaelB24@yahoo.com>
2019-05-16 18:32:10 -0700

    Add User signup test

--- spec/features/users_features_spec.rb
+++ spec/features/users_features_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 
-RSpec.describe User do
-  
+RSpec.describe 'Users features', :type => :feature do
+  it 'signs up sucessfully' do
+    visit new_user_path
+    fill_in 'Username', :with => 'test'
+    fill_in 'Password', :with => 'test123'
+    click_button 'Sign Up'
+    expect(User.last.username).to eq('test')
+    expect(page).to have_content('Signed Up Successfully')
+  end
 end
