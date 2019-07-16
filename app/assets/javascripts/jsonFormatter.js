7638f01705165b460e07e0cf82a1b1697e7f918c
Rafael <RafaelB24@yahoo.com>
2019-07-15 23:09:42 -0400

    Correctly parse recipes with username JSONFormatter method getJSON

--- app/assets/javascripts/jsonFormatter.js
+++ app/assets/javascripts/jsonFormatter.js
@@ -1,10 +1,11 @@
 class JSONFormatter {
 
   static getJSON(url){
+    const userRecipes = [];
     $.get(url, (data) => {
-      console.log(data)
-    })
+      for(let i = 0; i < data.length; i++)
+      userRecipes.push({"user": data[i].user.username, "recipeName": data[i].name, "macros": `Protien: ${data[i].protien}g Carbs: ${data[i].carbs}g Fat: ${data[i].fat}g`})
+    });
+    return userRecipes;
   }
 }
-
-// Recipe Name protien carbs fat cooktime 
