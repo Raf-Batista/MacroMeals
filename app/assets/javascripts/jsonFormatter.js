827edfc0d84470a9bbae0aa73d773e9f5b7d32fa
Rafael <RafaelB24@yahoo.com>
2019-07-18 21:48:28 -0400

    Add event listener to links added to Recipes index via Ajax

--- app/assets/javascripts/jsonFormatter.js
+++ app/assets/javascripts/jsonFormatter.js
@@ -4,7 +4,7 @@ class JSONFormatter {
     let userRecipes = [];
     for(let i = 0; i < data.length; i++){
       let recipe = data[i];
-      userRecipes.push({"id": recipe.id, "data": `<li>${recipe.user.username} <a href="/recipes/${recipe.id}">${recipe.name}</a> ${recipe.macros}</li>`})
+      userRecipes.push({"id": recipe.id, "data": `<li><a href="/recipes/${recipe.id}">${recipe.name}</a> ${recipe.macros}</li>`})
 
     }
     return userRecipes;
