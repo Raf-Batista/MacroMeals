87d82ccefdb507012fe36f5aaeb9926d9ff1c770
Rafael <RafaelB24@yahoo.com>
2019-07-19 19:35:07 -0400

    Add event listener to links added from AJAX calls which adds a recipe to the page

--- app/assets/javascripts/jsonFormatter.js
+++ app/assets/javascripts/jsonFormatter.js
@@ -4,13 +4,13 @@ class JSONFormatter {
     let userRecipes = [];
     for(let i = 0; i < data.length; i++){
       let recipe = data[i];
-      userRecipes.push({"id": recipe.id, "data": `<li><a href="/recipes/${recipe.id}">${recipe.name}</a> ${recipe.macros}</li>`})
+      userRecipes.push({"id": recipe.id, "data": `<li><a href="/recipes/${recipe.id}.json">${recipe.name}</a> ${recipe.macros}</li>`})
 
     }
     return userRecipes;
   }
 
   static parseJSONShow(data){
-    return `<li>${data.name} ${data.macros} ${data.ingredients} ${data.prep_time} ${data.cook_time} ${data.directions}</li>`}`
+    return `<li>${data.name} ${data.macros} ${data.ingredients} ${data.prep_time} ${data.cook_time} ${data.directions}</li>`
   }
 }
